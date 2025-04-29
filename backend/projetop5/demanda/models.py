bb5ecebf02392aa8539bd457967c1a9ac81923fc
Luan Scolfaro Amorim Carneiro <scolfaroluan@gmail.com>
2025-04-29 14:24:01 -0300

    mudanca models reclamacao

--- backend/projetop5/demanda/models.py
+++ backend/projetop5/demanda/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 
-class Reclamacao(models.Model):
+class BaseModelQuerySet(models.QuerySet):
+  def delete(self):
+    self.update( deleted_at =timezone.now(), is_active =False)
+
+class BaseManager(models.Manager):
+  def get_queryset (self):
+    return BaseModelQuerySet( self.model, using=self._db).filter( deleted_at__isnull =True, is_active =True)
+
+class BaseModel(models.Model):
+  class Meta:
+    abstract = True
+
+  created_at = models.DateTimeField( auto_now_add = True)
+  updated_at = models.DateTimeField( auto_now = True)
+  deleted_at = models.DateTimeField( editable = False, blank=True, null=True)
+  is_active = models.BooleanField( editable = False, default=True)
+
+  objects = BaseManager()
+
+  def delete(self, kwargs):
+      self.is_active = False
+      self.deleted_at = timezone.now()
+      self.save()
+
+  def hard_delete (self, kwargs):
+      super(BaseModel , self).delete(**kwargs)
+
+class Reclamacao(BaseModel):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     telefone = models.CharField(max_length=15)
@@ -9,3 +36,4 @@ class Reclamacao(models.Model):
 
     def __str__(self):
         return f"Reclamação de {self.nome}"
+
