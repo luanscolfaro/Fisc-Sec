ffdba070fcdcac7ada7bb80d6cb3ca1b536cd52d
Luan Scolfaro Amorim Carneiro <scolfaroluan@gmail.com>
2025-05-16 18:48:38 -0300

    alterações para conectar ao front

--- backend/projetop5/projetop5/settings.py
+++ backend/projetop5/projetop5/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'usuario',
     'home',
     'behave_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
